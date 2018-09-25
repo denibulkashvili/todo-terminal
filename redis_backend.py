8c346200d4224a01141703830e944a373164888e
denibulkashvili <denibulkashvili@gmail.com>
2018-09-25 21:40:45 +0700

    Add redis_backend.py
    * Add utf-8 decoding
    *Add save and load from redis functions

--- redis_backend.py
+++ redis_backend.py
@@ -1,6 +1,27 @@
 import redis
+from colors import CEND, CGREEN, CRED
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+# redis connetion
+r = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
 
-def save_tasks_to_redis(task):
-    pass
+
+def save_tasks_to_redis(task_list):
+    """Save task to redis."""
+    try:
+        r.delete("todo:task")
+        for task in task_list:
+            r.lpush("todo:task", task)
+        print(CGREEN + "\n\t All unfinished tasks have been saved.",
+                        "See you next time!\n" + CEND)
+    except Exception as e:
+        print(e)
+        print(CRED + "\n\t Couldn't save your tasks! Sorry! \n" + CEND)
+
+def load_tasks_from_redis():
+    """Load data from redis."""
+    loaded_tasks = []
+    try:
+        loaded_tasks = r.lrange("todo:task", 0, -1)
+    except Exception as e:
+        print(CRED + e + CEND)
+    return loaded_tasks
